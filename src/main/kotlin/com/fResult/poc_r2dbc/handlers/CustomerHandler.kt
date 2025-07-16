9e46f75f1bb97d2fa6b1603d8eb9723d98a73264
fResult <styxmaz@gmail.com>
2025-07-17 02:58:27 +0700

    Create `CustomerHandler#createCustomer` and implement.

---
+++ src/main/kotlin/com/fResult/poc_r2dbc/handlers/CustomerHandler.kt
@@ -0,0 +1,19 @@
+package com.fResult.poc_r2dbc.handlers
+
+import com.fResult.poc_r2dbc.entities.Customer
+import com.fResult.poc_r2dbc.repositories.mongodb.CustomerRepository
+import kotlinx.coroutines.reactor.awaitSingle
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.bodyToMono
+import java.net.URI
+
+@Component
+class CustomerHandler(private val repository: CustomerRepository) {
+  suspend fun createCustomer(request: ServerRequest): ServerResponse =
+    request.bodyToMono<Customer>()
+      .flatMap(repository::save)
+      .flatMap { ServerResponse.created(URI.create("/customers/${it.id}")).bodyValue(it) }
+      .awaitSingle()
+}
